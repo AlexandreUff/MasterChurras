2efe3bb8062567ab284ee2c92d533cf21b4cfb42
AlexandreUff <97979545+AlexandreUff@users.noreply.github.com>
2022-01-22 15:16:36 -0300

    Atualização do index e inserção do JS

--- meuCodigo.js
+++ meuCodigo.js
@@ -1,1 +1,38 @@
-console.log('fois');
+function take(query) {
+    return document.querySelector(query);
+}
+
+function mensagemErro(elemento,mensagem) {
+    const campoMensagem = document.createElement('div');
+    campoMensagem.classList.add('mensagemErro');
+    campoMensagem.innerHTML = mensagem;
+    elemento.parentNode.after(campoMensagem);
+}
+
+function validarNome(n){
+    if(n.value === ''){
+        console.log('Tá vazio isso ae!');
+        n.classList.add('input-invalido');
+
+        mensagemErro(n,'O campo de nome não foi preenchido corretamente.');
+    } else {
+        console.log('Agora sim');
+    }
+}
+
+function validarEmail(e) {
+}
+
+function envioDeFormulario(e) {
+    e.preventDefault();
+
+    const nome = take('#meuNome');
+    const email = take('#meuEmail');
+
+    validarNome(nome);
+    validarEmail(email);
+}
+
+const formulario = take('.formulario');
+
+formulario.addEventListener('submit',envioDeFormulario);
