1d862882b0598fb8978e5676c8be03038564199f
AlexandreUff <97979545+AlexandreUff@users.noreply.github.com>
2022-01-24 13:15:48 -0300

    Inserção da Mensagem de Confirmação
    E remoção da expressão regular (por enquanto).

--- meuCodigo.js
+++ meuCodigo.js
@@ -20,23 +20,50 @@ function validarNome(n){
             //As únicas retrições ao nome são apenas essas mesmas.
     if(n.value === ''){
         mensagemErro(n,'O campo de nome não foi preenchido.');
+        return false;
     } else if (n.value.length > 50){
         mensagemErro(n,'O campo de nome deve conter até 50 caracteres.');
+        return false;
     } 
+    return true;
 }
 
 function validarEmail(e) {
 
-    const regexEmail = /[a-zA-z0-9.]+@[a-zA-z0-9]+.?([a-z])*\.[a-z]{1,3}/g;
+    //const regexEmail = /[a-zA-z0-9.]+@[a-zA-z0-9]+.?([a-z])*\.[a-z]{1,3}/g;
 
     if(e.value === ''){
         mensagemErro(e,'O campo de e-mail não foi preenchido.');
-    } else if (e.value.length > 100){
-        mensagemErro(e,'O campo de nome deve conter até 100 caracteres.');
-    } else if (!regexEmail.test(e.value)){
+        return false;
+    } else if (e.value.length > 320){
+        mensagemErro(e,'O campo de nome deve conter até 320 caracteres.');
+        return false;
+    } /* else if (!regexEmail.test(e.value)){
         mensagemErro(e,'Endereço de e-mail inválido.');
+        return false;
         console.log(regexEmail.test(e))
+    } */
+        return true
+}
+
+function confirmacaoDeEnvio(){
+    const msgBG = document.createElement('div');
+    msgBG.onclick = function() {
+        this.parentNode.removeChild(this);
     }
+    msgBG.classList.add('fundo-mensagem');
+
+    const msgPopUp = document.createElement('div');
+    msgPopUp.classList.add('pop-up-mensagem');
+    msgPopUp.innerHTML = `<p class="titulo"> <strong>Mensagem de confirmação!</strong> </p>
+                            <p> Parabéns! Dentro de alguns minutos você receberá 
+                            o nosso e-mail de confirmação e, de cara, receberá
+                            as dicas Master Churras da semana! </p>`;
+
+    msgBG.appendChild(msgPopUp);
+
+    const body = take('body');
+    body.appendChild(msgBG);
 }
 
 function envioDeFormulario(e) {
@@ -45,8 +72,8 @@ function envioDeFormulario(e) {
     const nome = take('#meuNome');
     const email = take('#meuEmail');
 
-    validarNome(nome);
-    validarEmail(email);
+    if(validarNome(nome) & validarEmail(email)) confirmacaoDeEnvio();
+    /* validarEmail(email); */
 }
 
 const formulario = take('.formulario');
